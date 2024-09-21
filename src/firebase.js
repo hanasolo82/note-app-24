f67c9d12aa52f3a3ffcc7c150806faabdece587d
Hanasolo <hanasolo@hotmail.com>
2024-09-21 11:00:00 +0200

    resolvin some security problems

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection} from "firebase/firestore";
 
+const myApi = import.meta.env.VITE_API_KEY;
+const messageSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_APP_ID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLLs-FvO8Cs8NldFiuGMGUfbm5Q4mIL2o",
+  apiKey: myApi,
   authDomain: "notesapp-24.firebaseapp.com",
   projectId: "notesapp-24",
   storageBucket: "notesapp-24.appspot.com",
-  messagingSenderId: "260782492267",
-  appId: "1:260782492267:web:8f6e1bb25b65d87d473f2e"
+  messagingSenderId: messageSenderId,
+  appId: appId
 };
 
 const app = initializeApp(firebaseConfig);
